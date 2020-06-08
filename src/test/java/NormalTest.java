dd4c812793e9c8fd6dd33a5685f8911076bbe57b
beamrolling <beamrolling@outlook.com>
2020-06-08 23:02:49 +0800

    Merge two zero-quality test into one

--- src/test/java/NormalTest.java
+++ src/test/java/NormalTest.java
@@ -10,11 +10,15 @@ public class NormalTest {
     }
 
     @Test
-    public void should_return_zero_when_quality_is_zero_and_sellin_greater_than_zero() throws Exception {
-        Normal normal = new Normal(5,0);
-        normal.refresh();
-        assertEquals(normal.getSellIn(),4);
-        assertEquals(normal.getQuality(),0);
+    public void should_return_zero_when_quality_is_zero() throws Exception {
+        Normal positiveSellIn = new Normal(5,0);
+        Normal negativeSellIn = new Normal(-1,0);
+        positiveSellIn.refresh();
+        assertEquals(positiveSellIn.getSellIn(),4);
+        assertEquals(positiveSellIn.getQuality(),0);
+        negativeSellIn.refresh();
+        assertEquals(negativeSellIn.getSellIn(),-2);
+        assertEquals(negativeSellIn.getQuality(),0);
     }
 
     @Test
