5894892f769a61571b35b3a280dcb384be12da32
beamrolling <beamrolling@outlook.com>
2020-06-08 21:20:16 +0800

    Construct normal and backstagePass class, add corresponding test

---
+++ src/main/java/BackstagePass.java
@@ -0,0 +1,25 @@
+public class BackstagePass {
+    private int sellIn;
+    private int quality;
+
+    public BackstagePass(int sellIn, int quality) {
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
+
+    public int getSellIn() {
+        return sellIn;
+    }
+
+    public void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+}
---
+++ src/main/java/Normal.java
@@ -0,0 +1,25 @@
+public class Normal {
+    private int sellIn;
+    private int quality;
+
+    public Normal(int sellIn, int quality) {
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
+
+    public int getSellIn() {
+        return sellIn;
+    }
+
+    public void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+}
