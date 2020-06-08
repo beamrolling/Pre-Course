816b1d8fe0d0dbddd936bf789c8a4340c6d096ba
beamrolling <beamrolling@outlook.com>
2020-06-08 23:35:02 +0800

    when quality < 50 and sellin > 10

--- src/test/java/BackstagePassTest.java
+++ src/test/java/BackstagePassTest.java
@@ -16,4 +16,12 @@ public class BackstagePassTest {
         assertEquals(backstagePass.getSellIn(),-1);
         assertEquals(backstagePass.getQuality(),0);
     }
+
+    @Test
+    public void should_add_one_when_quality_less_than_fifty_and_sellin_greater_than_ten() throws Exception {
+        BackstagePass backstagePass = new BackstagePass(12,40);
+        backstagePass.refresh();
+        assertEquals(backstagePass.getSellIn(),11);
+        assertEquals(backstagePass.getQuality(),41);
+    }
 }
