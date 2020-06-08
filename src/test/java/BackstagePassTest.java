c91291fddeb59c40af7a7ae9f2454ccba67f1634
beamrolling <beamrolling@outlook.com>
2020-06-08 23:37:37 +0800

    remove exception thrown in test class

--- src/test/java/BackstagePassTest.java
+++ src/test/java/BackstagePassTest.java
@@ -3,14 +3,14 @@ import static org.junit.Assert.assertEquals;
 
 public class BackstagePassTest {
     @Test
-    public void should_return_backstagePass_sellin_and_quality() throws Exception {
+    public void should_return_backstagePass_sellin_and_quality() {
         BackstagePass backstagePass = new BackstagePass(1,1);
         assertEquals(backstagePass.getSellIn(),1);
         assertEquals(backstagePass.getQuality(),1);
     }
 
     @Test
-    public void should_return_zero_when_sellin_less_than_zero() throws Exception {
+    public void should_return_zero_when_sellin_less_than_zero() {
         BackstagePass backstagePass = new BackstagePass(0,50);
         backstagePass.refresh();
         assertEquals(backstagePass.getSellIn(),-1);
@@ -18,7 +18,7 @@ public class BackstagePassTest {
     }
 
     @Test
-    public void should_add_one_when_quality_less_than_fifty_and_sellin_greater_than_ten() throws Exception {
+    public void should_add_one_when_quality_less_than_fifty_and_sellin_greater_than_ten() {
         BackstagePass backstagePass = new BackstagePass(12,40);
         backstagePass.refresh();
         assertEquals(backstagePass.getSellIn(),11);
--- src/test/java/NormalTest.java
+++ src/test/java/NormalTest.java
@@ -3,14 +3,14 @@ import static org.junit.Assert.assertEquals;
 
 public class NormalTest {
     @Test
-    public void should_return_normal_sellin_and_quality() throws Exception {
+    public void should_return_normal_sellin_and_quality() {
         Normal normal = new Normal(1,1);
         assertEquals(normal.getSellIn(),1);
         assertEquals(normal.getQuality(),1);
     }
 
     @Test
-    public void should_return_zero_when_quality_is_zero() throws Exception {
+    public void should_return_zero_when_quality_is_zero() {
         Normal positiveSellIn = new Normal(5,0);
         Normal negativeSellIn = new Normal(-1,0);
         positiveSellIn.refresh();
@@ -22,7 +22,7 @@ public class NormalTest {
     }
 
     @Test
-    public void should_subtract_one_when_quality_greater_than_zero_and_sellin_greater_than_zero() throws Exception {
+    public void should_subtract_one_when_quality_greater_than_zero_and_sellin_greater_than_zero() {
         Normal normal = new Normal(5,3);
         normal.refresh();
         assertEquals(normal.getSellIn(),4);
@@ -30,7 +30,7 @@ public class NormalTest {
     }
 
     @Test
-    public void should_return_zero_when_quality_less_than_veloocity_and_sellin_less_than_zero() throws Exception {
+    public void should_return_zero_when_quality_less_than_veloocity_and_sellin_less_than_zero() {
         Normal normal = new Normal(-1,1);
         normal.refresh();;
         assertEquals(normal.getSellIn(),-2);
@@ -38,7 +38,7 @@ public class NormalTest {
     }
 
     @Test
-    public void should_subtract_velocity_when_when_quality_grater_than_veloocity_and_sellin_less_than_zero() throws Exception {
+    public void should_subtract_velocity_when_when_quality_grater_than_veloocity_and_sellin_less_than_zero() {
         Normal normal = new Normal(-1,3);
         normal.refresh();
         assertEquals(normal.getSellIn(),-2);
